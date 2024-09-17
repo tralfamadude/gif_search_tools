ff200d2e2b315840a5e4093952ead5af9be904b2
tralfamadude <tralfamadude@gmail.com>
2024-09-17 16:23:18 -0700

    make it easier to specify dir

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate gifcities_exp
 echo "ENV is $CONDA_DEFAULT_ENV"
+sleep 10
 BASE=full_jsonl
 
-for j in full_jsonl/*gz ; do  
-	INPUT="full_jsonl/$(basename $j .gz).jsonl"
-	OUTPUT="full_jsonl/$(basename $j .gz).embedding.jsonl"
+for j in ${BASE}/*gz ; do  
+	INPUT="${BASE}/$(basename $j .gz).jsonl"
+	OUTPUT="${BASE}/$(basename $j .gz).embedding.jsonl"
 	[ -r $OUTPUT ]  && echo "SKIP $INPUT: already processed" &&  continue
 	zcat $j > $INPUT   # unpack into input for next step
 	echo "-----------------${INPUT} -> ${OUTPUT}---------$(date '+%Y%m%dt%H%M')---"
