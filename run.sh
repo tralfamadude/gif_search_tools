ffe47a3e48a70f8311bb6422a4c64bf15589036d
tralfamadude <tralfamadude@gmail.com>
2024-09-24 20:08:52 -0700

    increase neighborhood size a bit

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ echo "ENV is $CONDA_DEFAULT_ENV"
 sleep 10
 BASE=full_jsonl
 
-for j in ${BASE}/*gz ; do  
-	INPUT="${BASE}/$(basename $j .gz).jsonl"
-	OUTPUT="${BASE}/$(basename $j .gz).embedding.jsonl"
+for j in  full_jsonl/*gz ; do  
+	INPUT="full_jsonl/$(basename $j .gz).jsonl"
+	OUTPUT="full_jsonl/$(basename $j .gz).embedding.jsonl"
 	[ -r $OUTPUT ]  && echo "SKIP $INPUT: already processed" &&  continue
 	zcat $j > $INPUT   # unpack into input for next step
 	echo "-----------------${INPUT} -> ${OUTPUT}---------$(date '+%Y%m%dt%H%M')---"
-	python json2img_embedding_standalone.py --output_file $OUTPUT $INPUT --model_name ViT-L-14 --pretrained laion2b_s32b_b82k  --k 3 --neighborhood_threshold 0.05  
+	python json2img_embedding_standalone.py --output_file $OUTPUT $INPUT --model_name ViT-L-14 --pretrained laion2b_s32b_b82k  --k 3 --neighborhood_threshold 0.08  
 done
